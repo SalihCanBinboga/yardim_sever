2be8166e3266924c6c7a12faa2dd3c26d4b67927
salih <salihcn.bnb@gmail.com>
2021-02-07 14:20:54 +0300

    image const. updated

--- lib/core/constants/image/image_constants.dart
+++ lib/core/constants/image/image_constants.dart
@@ -4,7 +4,9 @@ class ImageConstants {
   static ImageConstants get instance =>  _instance ??= ImageConstants._init();
   ImageConstants._init();
 
-  //String get image => toPng('image.png');
+  String get icGetHelp => toPng('get_help.png');
+  String get icHelp => toPng('help.png');
+  String get icAccount => toPng('account.png');
 
   String toPng(String name) => 'assets/images/$name';
 }
